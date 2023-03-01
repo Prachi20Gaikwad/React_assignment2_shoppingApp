c4fa433cb27986870546bd7170211f17a28d4156
Prachi Gaikwad <prachi.gaikwad@tigeranalytics.local>
2023-03-01 23:54:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
 import './App.css';
+import Products from './components/Products';
+import Product from './components/Product';
+import Home from './components/Home';
+import React, { useEffect, useState } from "react";
+import Navbar from './components/Navbar';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import About from './components/About';
+import Contact from './components/Contact';
+import jwt_decode from "jwt-decode";
 
 function App() {
+  const [user, setUser] = useState({});
+  const [IsSignedIn, setIsSignedIn] = useState(false);
+
+  function handleCallbackResponse(response) {
+    console.log("Encoded JWT ID token: " + response.credential);
+    var userObject = jwt_decode(response.credential);
+    console.log(userObject);
+    setUser(userObject);
+    setIsSignedIn(true);
+    document.getElementById("signInDiv").hidden = true;
+  }
+
+  function handleSignOut(event) {
+    setUser({});
+    setIsSignedIn(false);
+    document.getElementById("signInDiv").hidden = false;
+  }
+
+  useEffect(() => {
+    /*global google*/
+    google.accounts.id.initialize({
+      client_id:
+        "1024960509808-6rqdknpet2odqdf16mu86tvh6folv7d4.apps.googleusercontent.com",
+      callback: handleCallbackResponse,
+    });
+
+    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+      theme: "outline",
+      size: "large",
+    });
+    // google.accounts.id.prompt();
+  }, []);
+
+
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {!IsSignedIn && (
+        <div
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            margin: "auto",
+            display: "flex",
+            textAlign: "center",
+            justifyContent: "center",
+            height: "100vh",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h1>Sign In</h1>
+          <div id="signInDiv"></div>
+        </div>
+      )}
+      {IsSignedIn && (
+        <div>
+          <Navbar handleSignOut={handleSignOut} />
+
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/products" element={<Products />} />
+            <Route exact path="/cart" element={<Cart />} />
+            <Route exact path="/checkout" element={<Checkout />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/products/:id" element={<Product />} />
+          </Routes>
+        </div>
+
+      )
+      };
+
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+
+const Navbar = (props) => {
+    const state = useSelector((state) => state.handleCart)
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 shadow-sm">
+                <div className="container">
+                    <NavLink className="navbar-brand fw-bold fs-4" to="/">
+                        LA COLLECTION
+                    </NavLink>
+                    <button className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse"
+                        id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link active"
+                                    aria-current="page"
+                                    to="/">Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link"
+                                    to="/products">
+                                    Products
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink className="nav-link"
+                                    to="/about">
+                                    About
+                                </NavLink>
+                            </li>
+
+                            <li className="nav-item">
+                                <NavLink className="nav-link"
+                                    to="/contact">
+                                    Contact
+                                </NavLink>
+                            </li>
+
+
+                        </ul>
+                        <div className="buttons">
+                            {/* <button
+                                style={{
+                                    cursor: "pointer",
+                                    font: "inherit",
+                                    border: "none",
+                                    backgroundColor: "#015a53",
+                                    color: "white",
+                                    padding: "0.75rem 3rem",
+                                    display: "flex",
+                                    justifyContent: "space-around",
+                                    alignItems: "center",
+                                    borderRadius: "25px",
+                                    fontWeight: "bold",
+                                }}
+                                onClick={(e) => props.handleSignOut(e)}
+                            >
+                                {" "}
+                                Sign Out
+                            </button> */}
+                            <NavLink to="/login" className="btn btn-outline-dark" onClick={(e) => props.handleSignOut(e)}>
+                                <i className="fa fa-sign-in me-1"></i>Logout
+                            </NavLink>
+                            <NavLink to="/register" className="btn btn-outline-dark ms-2">
+                                <i className="fa fa-user-plus me-1"></i>Register
+                            </NavLink>
+                            <NavLink to="/cart" className="btn btn-outline-dark ms-2">
+                                <span className="fa fa-shopping-cart me-1"></span>
+                                Cart ({state.length})
+                            </NavLink>
+                        </div>
+                    </div>
+                </div>
+            </nav >
+        </div >
+
+    );
+}
+
+export default Navbar;
